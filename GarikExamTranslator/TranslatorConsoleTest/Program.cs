92cced0524e7426f907d067954c29693f0dbd6f4
ProstoGarik <bbanan570@gmail.com>
2024-03-16 18:24:42 +0300

    Console Test Added

---
+++ GarikExamTranslator/TranslatorConsoleTest/Program.cs
@@ -0,0 +1,33 @@
+﻿using ExamTranslatorClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TranslatorConsoleTest
+{
+    internal class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            WordListClass myWordList = new WordListClass();
+            myWordList.AddWord("TestWord", "ТестСлово");
+            Console.WriteLine(myWordList.GetWordListCount());
+            Console.WriteLine(myWordList.GetWordByIndex(1).Word);
+            Console.WriteLine(myWordList.GetWordByIndex(1).Translation);
+            myWordList.TargetWordByIndex(1);
+            myWordList.EditWord("TestWordEdited", "ТестСловоРедактировано");
+            Console.WriteLine(myWordList.GetWordByIndex(1).Word);
+            Console.WriteLine(myWordList.GetWordByIndex(1).Translation);
+            myWordList.DeleteWord();
+            Console.WriteLine(myWordList.GetWordListCount());
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
